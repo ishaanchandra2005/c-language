1cd373e6e1ece38ab2cb892bf7e617ea4d3675db
ishaanchandra2005 <144019405+ishaanchandra2005@users.noreply.github.com>
2023-11-05 00:16:39 +0530

    Add files via upload

---
+++ week-5/arrays-21.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{	
+	int i,j,a[3][3];
+	printf("enter a matrix:\n");
+	for (i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+		scanf("%d",&a[i][j]);
+		}
+	}
+	printf("matrix elements:\n");
+	for (i=0;i<3;i++)
+	{
+	    for (j=0;j<3;j++)
+	{
+	printf("%d",a[i][j]);
+	
+	}
+	printf("\n");
+	}
+	return 0;
+}
---
+++ week-5/arrays-22.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,a[3][3],b[3][3],c[3][3];
+	printf("enter the first matrix:\n");
+	for (i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+			scanf("%d",& a[i][j]);
+		}
+	}
+	
+	printf("enter the second matrix:\n");
+	for (i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+			scanf("%d",& b[i][j]);
+		}
+	}
+	
+
+	printf("matrix obtained after addition is:\n");
+	for(i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+		c[i][j]=a[i][j]+b[i][j];
+		printf("%d",c[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("matrix obtained after substraction is:\n");
+	for(i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+		c[i][j]=a[i][j]-b[i][j];
+		printf("%d",c[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+
+}
