16314e1e63bd8d4869f894a7ecbefc9127610cf5
ishaanchandra2005 <144019405+ishaanchandra2005@users.noreply.github.com>
2023-11-04 17:23:57 +0530

    Add files via upload

---
+++ week-1/square and cube.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,sq,cb;
+	printf("enter value:");
+	scanf("%d",&a);
+	sq=(a*a);
+	cb=(a*a)*a;
+	printf("square is %d\n",sq);
+	printf("cube is %d",cb);
+	return 0;
+}
