1b45e66d1be39860d71c94b9957f1f43b95c735c
ishaanchandra2005 <144019405+ishaanchandra2005@users.noreply.github.com>
2023-11-05 23:06:54 +0530

    Update electricity bill.cpp

--- week-2/electricity bill.cpp
+++ week-2/electricity bill.cpp
@@ -7,7 +7,7 @@ int main ()
 	scanf("%d",a);
 	if (a>=0 && a<=100)
 	printf ("the bill is %d",a*2);
-	else if (u>100 && u <=250)
+	else if (a>100 && a <=250)
 	printf("the bill is %d",a*3);
 	else 
 	printf("the bill is %d",a*5);
